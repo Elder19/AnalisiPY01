004e7dc32294f4dd0bf1a4a90af0422078281009
ELDERPEREZ <125334333+Elder19@users.noreply.github.com>
2025-04-30 11:33:02 -0600

    doc

--- codigo/Backend/JsonM.py
+++ codigo/Backend/JsonM.py
@@ -1,5 +1,11 @@
 import json
-
+""" charge the matrix in the and save the matrix in the json file
+        Args:
+        Returns:
+           return datos in a list
+        Raises:
+            execpt if  FileNotFoundError,JSONDecodeError
+        """
 def cargarDatos():
     try:
         with open('datos.json', 'r') as archivo:
@@ -9,7 +15,13 @@ def cargarDatos():
             return datos
     except (FileNotFoundError, json.JSONDecodeError):
         return []
-
+""" write the matrix in the json file
+        Args:
+            lista_matrices: list of matrix to save in the json 
+        Returns:
+           return true if the matrix is saved
+        Raises:
+        """
 def escribirMatriz(lista_matrices):
     with open('datos.json', 'w') as archivo:
         json.dump(lista_matrices, archivo, indent=6)
